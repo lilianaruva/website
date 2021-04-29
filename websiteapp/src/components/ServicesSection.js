dc143183334b6103de565e0b566a15609cf29a8b
Liliana <lily.gallegos@hotmail.com>
2021-04-28 22:47:48 -0500

    Services Section
    Create services section component and add to About Us page

---
+++ websiteapp/src/components/ServicesSection.js
@@ -0,0 +1,51 @@
+import React from 'react';
+//icons.
+import clock from '../img/clock.svg';
+import diaphram from '../img/diaphragm.svg'
+import money from '../img/money.svg';
+import teamwork from '../img/teamwork.svg';
+import home2 from '../img/home2.png'
+
+const ServicesService = () => {
+    return (
+        <div className="services">
+            <div className="description">
+                <h2>High <span>quality</span> services</h2>
+                <div className="cards">
+                    <div className="card">
+                        <div className="icon">
+                            <img src={clock} alt="icon"></img>
+                            <h3>Efficent</h3>
+                        </div>
+                        <p>Lorem ipsum dolor sit amet, consectetur</p>
+                    </div>
+                    <div className="card">
+                        <div className="icon">
+                            <img src={teamwork} alt="icon"></img>
+                            <h3>Teamwork</h3>
+                        </div>
+                        <p>Lorem ipsum dolor sit amet, consectetur</p>
+                    </div>
+                    <div className="card">
+                        <div className="icon">
+                            <img src={diaphram} alt="icon"></img>
+                            <h3>Diaphram</h3>
+                        </div>
+                        <p>Lorem ipsum dolor sit amet, consectetur</p>
+                    </div>
+                    <div className="card">
+                        <div className="icon">
+                            <img src={money} alt="icon"></img>
+                            <h3>Affordable</h3>
+                        </div>
+                        <p>Lorem ipsum dolor sit amet, consectetur</p>
+                    </div>
+                </div>
+            </div>
+            <div className="image">
+                <img src={home2} alt="secondPhoto"></img>
+            </div>
+        </div>
+    )
+}
+export default ServicesService;
