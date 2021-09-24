1de1c77093c0cbbc862e7614a8d53137637cc647
Liliana <lily.gallegos@hotmail.com>
2021-09-24 18:54:21 -0500

    Contact Animation

--- src/pages/ContacUs.js
+++ src/pages/ContacUs.js
@@ -1,21 +1,76 @@
 import React from "react";
 //Animations
 import { motion } from "framer-motion";
-import { pageAnimation } from "../animation";
+import { pageAnimation, titleAnim } from "../animation";
+import styled from "styled-components";
 
 const ContactUs = () => {
   return (
     <>
-      <motion.div
+      <ContactStyle
         exit="exit"
         variants={pageAnimation}
         initial="hidden"
         animate="show"
       >
-        <h1>Our CONTACT</h1>
-      </motion.div>
+        <Title>
+          <Hide>
+            <motion.h2 variants={titleAnim}>Get it touch.</motion.h2>
+          </Hide>
+        </Title>
+        <div>
+          <Hide>
+            <Social variants={titleAnim}>
+              <Circle />
+              <h2>Send Us A Message</h2>
+            </Social>
+          </Hide>
+          <Hide>
+            <Social variants={titleAnim}>
+              <Circle />
+              <h2>Send An Email</h2>
+            </Social>
+          </Hide>
+          <Hide>
+            <Social variants={titleAnim}>
+              <Circle />
+              <h2>Send Us A Message</h2>
+            </Social>
+          </Hide>
+        </div>
+      </ContactStyle>
     </>
   );
 };
+
+const ContactStyle = styled(motion.div)`
+  padding: 5rem 10rem;
+  color: white;
+  min-height: 90vh;
+`;
+
+const Title = styled.div`
+  margin-bottom: 4rem;
+  color: white;
+`;
+
+const Hide = styled.div`
+  overflow: hidden;
+`;
+
+const Circle = styled.div`
+  border-radius: 50%;
+  width: 3rem;
+  height: 3rem;
+  background: white;
+`;
+
+const Social = styled(motion.div)`
+  display: flex;
+  align-items: center;
+  h2 {
+    margin: 2rem;
+  }
+`;
 
 export default ContactUs;
