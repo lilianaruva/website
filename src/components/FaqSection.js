03d7136f4173a6372c7bfeb731a4a43040ecc7eb
Liliana <lily.gallegos@hotmail.com>
2021-09-23 19:37:11 -0500

    Scroll Animation

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -3,11 +3,19 @@ import styled from "styled-components";
 import { About } from "../styles";
 import Toggle from "./Toggle";
 import { AnimateSharedLayout } from "framer-motion";
+import { useScroll } from "./useScroll";
+import { scrollReveal } from "../animation";
 
 const FaqSection = () => {
+  const [element, controls] = useScroll();
   return (
     <>
-      <Faq>
+      <Faq
+        ref={element}
+        animate={controls}
+        initial="hidden"
+        variants={scrollReveal}
+      >
         <h2>
           Any Questions <span>FAQ</span>
         </h2>
--- src/components/ServicesSection.js
+++ src/components/ServicesSection.js
@@ -1,82 +1,92 @@
-import React from 'react';
+import React from "react";
 //icons.
-import clock from '../img/clock.svg';
-import diaphram from '../img/diaphragm.svg'
-import money from '../img/money.svg';
-import teamwork from '../img/teamwork.svg';
-import home2 from '../img/home2.png'
+import clock from "../img/clock.svg";
+import diaphram from "../img/diaphragm.svg";
+import money from "../img/money.svg";
+import teamwork from "../img/teamwork.svg";
+import home2 from "../img/home2.png";
 //Styles
-import {About, Description, Image} from "../styles";
+import { About, Description, Image } from "../styles";
 import styled from "styled-components";
+import { scrollReveal } from "../animation";
+import { useScroll } from "./useScroll";
 
 const ServicesService = () => {
-    return (
-        <Services>
-            <Description>
-                <h2>High <span>quality</span> services</h2>
-                <Cards>
-                    <Card>
-                        <div className="icon">
-                            <img src={clock} alt="icon"></img>
-                            <h3>Efficent</h3>
-                        </div>
-                        <p>Lorem ipsum dolor sit amet, consectetur</p>
-                    </Card>
-                    <Card>
-                        <div className="icon">
-                            <img src={teamwork} alt="icon"></img>
-                            <h3>Teamwork</h3>
-                        </div>
-                        <p>Lorem ipsum dolor sit amet, consectetur</p>
-                    </Card>
-                    <Card>
-                        <div className="icon">
-                            <img src={diaphram} alt="icon"></img>
-                            <h3>Diaphram</h3>
-                        </div>
-                        <p>Lorem ipsum dolor sit amet, consectetur</p>
-                    </Card>
-                    <Card>
-                        <div className="icon">
-                            <img src={money} alt="icon"></img>
-                            <h3>Affordable</h3>
-                        </div>
-                        <p>Lorem ipsum dolor sit amet, consectetur</p>
-                    </Card>
-                </Cards>
-            </Description>
-            <Image>
-                <img src={home2} alt="secondPhoto"></img>
-            </Image>
-        </Services>
-    );
+  const [element, controls] = useScroll();
+  return (
+    <Services
+      ref={element}
+      animate={controls}
+      initial="hidden"
+      variants={scrollReveal}
+    >
+      <Description>
+        <h2>
+          High <span>quality</span> services
+        </h2>
+        <Cards>
+          <Card>
+            <div className="icon">
+              <img src={clock} alt="icon"></img>
+              <h3>Efficent</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet, consectetur</p>
+          </Card>
+          <Card>
+            <div className="icon">
+              <img src={teamwork} alt="icon"></img>
+              <h3>Teamwork</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet, consectetur</p>
+          </Card>
+          <Card>
+            <div className="icon">
+              <img src={diaphram} alt="icon"></img>
+              <h3>Diaphram</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet, consectetur</p>
+          </Card>
+          <Card>
+            <div className="icon">
+              <img src={money} alt="icon"></img>
+              <h3>Affordable</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet, consectetur</p>
+          </Card>
+        </Cards>
+      </Description>
+      <Image>
+        <img src={home2} alt="secondPhoto"></img>
+      </Image>
+    </Services>
+  );
 };
 
 const Services = styled(About)`
-    h2{
-        padding-bottom: 5rem;
-    }
-    p{
-        width: 70%;
-        padding: 2rem 0rem 4rem 0rem;
-    }
+  h2 {
+    padding-bottom: 5rem;
+  }
+  p {
+    width: 70%;
+    padding: 2rem 0rem 4rem 0rem;
+  }
 `;
 const Cards = styled.div`
-    display: flex;
-    flex-wrap: wrap;
+  display: flex;
+  flex-wrap: wrap;
 `;
 const Card = styled.div`
-    flex-basis: 20rem;
-    .icon{
-        display: flex;
-        align-items: center;
-        h3{
-            margin-left: 1rem;
-            background: white;
-            color: black;
-            padding: 1rem;
-        }
+  flex-basis: 20rem;
+  .icon {
+    display: flex;
+    align-items: center;
+    h3 {
+      margin-left: 1rem;
+      background: white;
+      color: black;
+      padding: 1rem;
     }
-`
+  }
+`;
 
 export default ServicesService;
